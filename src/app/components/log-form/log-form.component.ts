5ebe2903389fcd2dc9fe28284d7635475674c9b8
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-09-16 20:19:05 +1000

    added non-persisted CRUD

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LogService } from '../../services/log.service';
 
+import { UUID } from 'angular2-uuid';
+
 @Component({
   selector: 'app-log-form',
   templateUrl: './log-form.component.html',
@@ -10,6 +12,7 @@ export class LogFormComponent implements OnInit {
   id: string;
   text: string;
   date: any;
+  isNew = true;
 
   constructor(private logService: LogService) {}
 
@@ -17,10 +20,34 @@ export class LogFormComponent implements OnInit {
     // Subscribe to the selected log observable
     this.logService.selectedLog.subscribe(log => {
       if (log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
     });
+  }
+
+  onSubmit() {
+    // Check if new log
+    if (this.isNew) {
+      // Create a new log
+      const newLog = {
+        id: UUID.UUID(),
+        text: this.text,
+        date: new Date(),
+      };
+      // Add Log
+      this.logService.addLog(newLog);
+    } else {
+      // Create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date(),
+      };
+      // Update Log
+      this.logService.updateLog(updLog);
+    }
   }
 }
